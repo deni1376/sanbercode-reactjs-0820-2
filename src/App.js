3e21319b2bb638ef7af0d8d33335365ed1c2e9c6
deni1376 <denim1376@gmail.com>
2020-09-10 07:38:33 +0700

    Tugas 13

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 //import Tugas9 from './Tugas-9/tugas9.js'
 //import DaftarBuah from './Tugas-10/tugas10.js';
 //import Timer from './Tugas-11/timer.js'
-import Lists from './Tugas-12/listForm.js'
+//import Lists from './Tugas-12/listForm.js'
+import Example from './Tugas-13/hooksApi.js'
 
 function App() {
   return (
     <div>
-      <Lists/>
+      <Example/>
     </div>
   );
 }
